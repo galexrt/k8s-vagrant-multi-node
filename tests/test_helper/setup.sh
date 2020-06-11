262d36c10b3a14f84839494d7776a2430ea3b0c7
Alexander Trost <galexrt@googlemail.com>
2020-06-11 10:07:30 +0200

    ci: debug groups of user
    Signed-off-by: Alexander Trost <galexrt@googlemail.com>

--- tests/test_helper/setup.sh
+++ tests/test_helper/setup.sh
@@ -14,26 +14,28 @@ mkdir -p .test_files/
 cd .test_files/ || { echo "Unable to cd to .test_files/ dir"; exit 1; }
 
 VAGRANT_DEFAULT_PROVIDER="${VAGRANT_DEFAULT_PROVIDER:-libvirt}"
-VAGRANT_DEFAULT_PROVIDER="$(echo "$VAGRANT_DEFAULT_PROVIDER" | tr '[:upper:]' '[:lower:]')"
+VAGRANT_DEFAULT_PROVIDER="$(echo "${VAGRANT_DEFAULT_PROVIDER}" | tr '[:upper:]' '[:lower:]')"
 
 VAGRANT_VERSION="${VAGRANT_VERSION:-2.2.9}"
 VIRTUALBOX_VERSION="${VIRTUALBOX_VERSION:-6.0}"
 
 echo "Installing Vagrant $VAGRANT_VERSION ..."
-curl -Os "https://releases.hashicorp.com/vagrant/${VAGRANT_VERSION}/vagrant_${VAGRANT_VERSION}_x86_64.deb"
-curl -Os "https://releases.hashicorp.com/vagrant/${VAGRANT_VERSION}/vagrant_${VAGRANT_VERSION}_SHA256SUMS"
-curl -Os "https://releases.hashicorp.com/vagrant/${VAGRANT_VERSION}/vagrant_${VAGRANT_VERSION}_SHA256SUMS.sig"
-gpg --receive-key 51852D87348FFC4C
-gpg --verify "vagrant_${VAGRANT_VERSION}_SHA256SUMS.sig" "vagrant_${VAGRANT_VERSION}_SHA256SUMS"
-sha256sum -c "vagrant_${VAGRANT_VERSION}_SHA256SUMS" 2>&1 | grep OK
+if [ ! -f "vagrant_${VAGRANT_VERSION}_x86_64.deb" ]; then
+    curl -Os "https://releases.hashicorp.com/vagrant/${VAGRANT_VERSION}/vagrant_${VAGRANT_VERSION}_x86_64.deb"
+    curl -Os "https://releases.hashicorp.com/vagrant/${VAGRANT_VERSION}/vagrant_${VAGRANT_VERSION}_SHA256SUMS"
+    curl -Os "https://releases.hashicorp.com/vagrant/${VAGRANT_VERSION}/vagrant_${VAGRANT_VERSION}_SHA256SUMS.sig"
+    gpg --receive-key 51852D87348FFC4C
+    gpg --verify "vagrant_${VAGRANT_VERSION}_SHA256SUMS.sig" "vagrant_${VAGRANT_VERSION}_SHA256SUMS"
+    sha256sum -c "vagrant_${VAGRANT_VERSION}_SHA256SUMS" 2>&1 | grep OK
+fi
 sudo apt-get update
 sudo apt-get install -y bridge-utils dnsmasq-base ebtables
 sudo dpkg -i "vagrant_${VAGRANT_VERSION}_x86_64.deb"
 
-echo "Vagrant installed $VAGRANT_VERSION."
+echo "Installed Vagrant ${VAGRANT_VERSION}."
 vagrant version
 
-case "$VAGRANT_DEFAULT_PROVIDER" in
+case "${VAGRANT_DEFAULT_PROVIDER}" in
     libvirt)
         echo "Installing libvirt"
         sudo apt-get install -y libvirt-bin libvirt-dev qemu-kvm qemu-utils ruby-dev
@@ -52,12 +54,13 @@ case "$VAGRANT_DEFAULT_PROVIDER" in
         echo "Installed virtualbox"
     ;;
     *)
-        echo "Unknown VAGRANT_DEFAULT_PROVIDER (value: $VAGRANT_DEFAULT_PROVIDER) given. Continuing for now"
+        echo "Unknown VAGRANT_DEFAULT_PROVIDER (value: ${VAGRANT_DEFAULT_PROVIDER}) given. Continuing for now"
     ;;
 esac
 
 echo "Installing kubectl"
-curl -LO "https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl"
-chmod +x ./kubectl
-sudo mv ./kubectl /usr/local/bin/kubectl
-echo "Installed kubectl"
+KUBERNETES_VERSION="$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)"
+curl -Lo "kubectl-${KUBERNETES_VERSION}" "https://storage.googleapis.com/kubernetes-release/release/${KUBERNETES_VERSION}/bin/linux/amd64/kubectl"
+chmod +x "kubectl-${KUBERNETES_VERSION}"
+sudo cp "kubectl-${KUBERNETES_VERSION}" /usr/local/bin/kubectl
+echo "Installed kubectl ${KUBERNETES_VERSION}"
