643c6b67c5bba1c0dc8d40fb646ca9805330f8f9
Alexander Trost <galexrt@googlemail.com>
2020-10-30 12:34:45 +0100

    ci: use different git diff for "check if cirun needed" check
    Signed-off-by: Alexander Trost <galexrt@googlemail.com>

--- tests/test_helper/check_if_cirun_needed.sh
+++ tests/test_helper/check_if_cirun_needed.sh
@@ -8,7 +8,7 @@ while read -r file; do
         SKIP_BUILD=False
         break
     fi
-done < <(git diff --name-only master..."${TRAVIS_COMMIT}")
+done < <(git diff --name-only --diff-filter=AM HEAD..."$TRAVIS_BRANCH")
 
 if [[ $SKIP_BUILD == True ]]; then
     echo "Only changed files that are ignored have been found. Terminating Travis run."
